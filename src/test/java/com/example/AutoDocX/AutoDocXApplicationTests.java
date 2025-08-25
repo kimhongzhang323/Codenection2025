15eaf579567164fbe8c0efb4306bd0e8984d8b5f
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2025-08-26 00:18:09 +0800

    Initial commit

---
+++ src/test/java/com/example/AutoDocX/AutoDocXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutoDocX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDocXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
