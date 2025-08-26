fad190671feb75657e378b6db7a3421a539a7656
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2025-08-26 21:35:53 +0800

    Updated model api

--- src/main/java/com/example/AutoDocX/AutoDocXApplication.java
+++ src/main/java/com/example/AutoDocX/AutoDocXApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutoDocXApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AutoDocXApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDocXApplication.class, args);
+    }
 }
