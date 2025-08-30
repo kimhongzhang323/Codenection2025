4ef3bbe95dcb4f7a0a2fd793eb4f9dda4f7bfd9b
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2025-08-31 02:35:53 +0800

    Update project configuration and security settings; add LFS support for model weights

--- src/main/java/com/example/AutoDocX/AutoDocXApplication.java
+++ src/main/java/com/example/AutoDocX/AutoDocXApplication.java
@@ -2,8 +2,9 @@ package com.example.AutoDocX;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AutoDocXApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutoDocXApplication.class, args);
